e9e8acbc89b3309d24cb90ac71b96ba23209a1bd
Praveen Tekuri <praveent3373@gmail.com>
2025-12-17 22:26:21 +0530

    2. Let's Travel: Admin page UI

---
+++ src/public/admin/js/main.js
@@ -0,0 +1,4 @@
+// simulate create post
+let addPostBtn = document.querySelector(".add-post-btn");
+let createPostBtn = document.getElementById("v-pills-add-post-tab");
+addPostBtn.addEventListener("click", () => createPostBtn.click());
