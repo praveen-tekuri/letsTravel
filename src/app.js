aa3ba8774e5f740df3443bfa7d156ada7b20b03c
Praveen Tekuri <praveent3373@gmail.com>
2025-12-17 23:19:10 +0530

    3. Let's Travel: DB connection | mongoose | nodemon | dotenv

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
+const {connectDB} = require("./config/database");
 
 app.use(express.static('public'));
 
-app.listen(3000, () => console.log("Server is listening on port 3000..."));
+connectDB().then(() => {
+    console.log("Database connection established!");
+    app.listen(process.env.PORT, () => console.log("Server is listening on port 3000..."));
+}).catch((error) => {
+    console.log("Database connection failed: " + error);
+})
